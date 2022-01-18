120bb542f31db4da5a96800f6a17fd1f4e430d52
Jay Butera <buterajay@protonmail.com>
2022-01-18 12:46:44 -0600

    Melorun accessible in flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,24 @@
 {
   description = "Mel Intermediate Lisp";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.naersk.url = "github:nix-community/naersk";
   inputs.mozilla = { url = "github:mozilla/nixpkgs-mozilla"; flake = false; };
+  inputs.melorun-repo = { url = "github:themeliolabs/melorun"; flake = false; };
 
   outputs =
     { self
     , nixpkgs
     , mozilla
     , flake-utils
-    , ...
+    , melorun-repo
+    , naersk
     } @inputs:
     let rustOverlay = final: prev:
           let rustChannel = prev.rustChannelOf {
             channel = "1.58.0";
-            sha256 = "sha256-6PfBjfCI9DaNRyGigEmuUP2pcamWsWGc4g7SNEHqD2c=";
+            sha256 = "sha256-eQBpSmy9+oHfVyPs0Ea+GVZ0fvIatj6QVhNhYKOJ6Jk=";
           };
           in
           { inherit rustChannel;
@@ -32,10 +35,18 @@
             rustOverlay
           ];
         };
+        naersk-lib = naersk.lib."${system}";
+        melorun = naersk-lib.buildPackage {
+          pname = "melorun";
+          root = melorun-repo;
+        };
+
         in {
           devShell = pkgs.mkShell {
             #RUST_BACKTRACE=1;
+
             buildInputs = with pkgs; [
+              melorun
               clang
               openssl
               (rustChannel.rust.override { extensions = [ "rust-src" ]; })
